993dc99c163c7760f47bc22fdca0f6c2516efd45
SALAUN Jeremie <jeremie.salaun@soprasteria.com>
2023-12-09 10:45:17 +0100

    Day 9 in single pass

--- AdventOfCode2023/Day9.cs
+++ AdventOfCode2023/Day9.cs
@@ -28,22 +28,16 @@ namespace AdventOfCode2023
                 }
             }
             long total1 = 0;
+            long total2 = 0;
             for(int i = 0; i < series.Count; i++)
             {
-                var n = FindNext(series[i]);
-                //Console.WriteLine($"Next for {string.Join(',',series[i])} is {n}");
+                Console.WriteLine();
+                var (p,n) = FindNextAndPrevious(series[i]);
+                Console.WriteLine($"{p} -> [{string.Join(',', series[i])}] -> {n}");
+                total2 += p;
                 total1 += n;
             }
             Console.WriteLine($"Final result for 1st star is : {total1}");
-
-            long total2 = 0;
-            for (int i = 0; i < series.Count; i++)
-            {
-                var n = FindPrevious(series[i]);
-                Console.WriteLine($"Previous for {string.Join(',', series[i])} is {n}");
-                total2 += n;
-            }
-
             Console.WriteLine($"Final result for 2nd star is : {total2}");
         }
 
@@ -53,35 +47,19 @@ namespace AdventOfCode2023
             return line.Split(' ').Select(long.Parse).ToList();
         }
 
-        private long FindNext(List<long> series)
+        private (long p,long n) FindNextAndPrevious(List<long> series)
         {
             if (series.TrueForAll(x => x == 0))
             {
-                //Console.WriteLine($"\tAnswer 0");
-                return 0;
+                Console.WriteLine($"\tAnswer (0,0)");
+                return (0,0);
             }
             else
             {
                 List<long> deltas = GetDeltas(series);
-                var d = FindNext(deltas);
-                //Console.WriteLine($"\tAnswer {series[series.Count - 1] + d}");
-                return series[series.Count - 1] + d;
-            }
-        }
-
-        private long FindPrevious(List<long> series)
-        {
-            if (series.TrueForAll(x => x == 0))
-            {
-                Console.WriteLine($"\tAnswer 0");
-                return 0;
-            }
-            else
-            {
-                List<long> deltas = GetDeltas(series);
-                var d = FindPrevious(deltas);
-                Console.WriteLine($"\tAnswer {series[0] - d}");
-                return series[0] - d;
+                var (d0,dn) = FindNextAndPrevious(deltas);
+                Console.WriteLine($"\tAnswer ({series[0] - d0},{series[series.Count - 1] + dn})");
+                return (series[0]-d0,series[series.Count - 1] + dn);
             }
         }
 
