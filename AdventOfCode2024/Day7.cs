88b5c198beaae1349e856d75b5e0098210e82003
SALAUN Jeremie <jeremie.salaun@soprasteria.com>
2024-12-07 10:54:45 +0100

    Day 7

--- AdventOfCode2024/Day7.cs
+++ AdventOfCode2024/Day7.cs
@@ -1,12 +1,4 @@
-﻿using AdventOfCode2024.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Data;
 
 namespace AdventOfCode2024
 {
@@ -20,6 +12,33 @@ namespace AdventOfCode2024
             long total2 = 0;
             //Read file
             var path = @"Inputs\day7.txt";
+            using (var sr = new StreamReader(path, true))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var x = ProcessLine(line);
+                    total1 += x;
+                    if (x > 0)
+                    {
+                        //No need to recalculate with || since we are OK with just the 2 first operators
+                        total2 += x;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\tMISSED without concat");
+                        x = ProcessLine(line, true);
+                        if (x > 0)
+                        {
+                            total2 += x;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\tMISSED");
+                        }
+                    }
+                }
+            }
 
 
             //Print out total result
@@ -30,6 +49,68 @@ Final result for 2nd star is : {total2}
             Thread.Sleep(1000);
         }
 
+        private long ProcessLine(string line, bool useConcat = false)
+        {
+            var colon = line.IndexOf(':');
+            var goal = long.Parse(line.Substring(0, colon));
+            var values = line.Substring(colon + 2).Split(' ').Select(s => long.Parse(s)).ToList();
+            Console.Write($"Goal is {goal}, values are {string.Join(',', values)} {(useConcat ? "(with concat)" : "")}");
+
+            //Shortcut : try some direct calculations
+            var lowest = GetLowest(values);
+            var highest = GetHighest(values,useConcat);
+            Console.Write($"\t result reachable within [{lowest},{highest}]");
+            if (goal < lowest || goal > highest)
+            {
+                Console.WriteLine("\t => GOAL OUT OF REACH !!");
+                return 0;
+            }
+            if (goal == lowest || goal == highest)
+            {
+                Console.WriteLine("\t => DIRECT FIND !!");
+                return goal;
+            }
+
+            if (Calculate(goal, values, 0, 0, "+", useConcat)) return goal;
+            if (Calculate(goal, values, 0, 0, "*", useConcat)) return goal;
+            if (useConcat && Calculate(goal, values, 0, 0, "||", useConcat)) return goal;
+            return 0;
+        }
+
+        private static long GetHighest(List<long> values, bool useConcat = false)
+        {
+            return values.Aggregate((i1, i2) =>
+            {
+                var l = Math.Max((i1 + i2), (i1 * i2));
+                if (useConcat)
+                {
+                    l = Math.Max(l, long.Parse(string.Concat(i1, i2)));
+                }
+                return l;
+            });
+        }
+
+        private static long GetLowest(List<long> values)
+        {
+            return values.Where(i => i != 1).Aggregate((i1, i2) => i1 + i2);
+        }
+
+        private bool Calculate(long goal, List<long> values, int currentIndex, long currentResult, string ope, bool useConcat = false)
+        {
+            if (currentIndex == values.Count)
+            {
+                if (currentResult == goal) Console.WriteLine("\t GOAL REACHED !");
+                return currentResult == goal;
+            }
+            var curVal = values[currentIndex];
+            if (ope == "+") currentResult += curVal;
+            if (ope == "*") currentResult *= curVal;
+            if (ope == "||") currentResult = long.Parse(string.Concat(currentResult, curVal));
+            if (currentResult > goal) return false; //Shortcut : goal is passed, no need to continue on this path
+            return Calculate(goal, values, currentIndex + 1, currentResult, "+", useConcat)
+                || Calculate(goal, values, currentIndex + 1, currentResult, "*", useConcat)
+                || (useConcat && Calculate(goal, values, currentIndex + 1, currentResult, "||", useConcat));
+        }
     }
 }
 
