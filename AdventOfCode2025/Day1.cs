9e908231df84c98a4e43419c2e7a4cd1db9950dd
SALAUN Jeremie <jeremie.salaun@soprasteria.com>
2025-12-01 10:04:18 +0100

    Day 1 of 2025

--- AdventOfCode2025/Day1.cs
+++ AdventOfCode2025/Day1.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,23 @@ namespace AdventOfCode2025
 {
     internal class Day1
     {
-        const bool TEST = true;
+
+        private class turn
+        {
+            public int dir = 1;
+            public int distance = 0;
+            public int move(int pos)
+            {
+                return (pos + dir * distance) % 100;
+            }
+
+            public int move2(int pos)
+            {
+                return (pos + dir * distance);
+            }
+        }
+
+        const bool TEST = false;
         public void Run()
         {
             Console.WriteLine("#################################### WELCOME TO DAY 1 ##################################\r\n");
@@ -20,7 +37,35 @@ namespace AdventOfCode2025
             long total2 = 0;
             //Read file
             var path = $@"{(TEST ? "Samples" : "Inputs")}\day1.txt";
+            var instructions = File.ReadAllLines(path).Select(s => new turn() { dir = (s[0] == 'L' ? -1 : 1), distance=(int.Parse(s.Substring(1))) }).ToList();
 
+            var curpos = 50;
+            foreach(var i in instructions)
+            {
+                curpos = i.move(curpos);
+                if (curpos == 0) total1++;
+            }
+
+            curpos = 50;
+            foreach (var i in instructions)
+            {
+                var newpos = i.move2(curpos);
+                int x;
+                if (newpos > curpos)
+                {
+                    x  = (newpos / 100) - (curpos/100);
+                    if (newpos > 0 && curpos < 0) x++;
+                }
+                else
+                {
+                    x = (curpos / 100) - (newpos / 100);
+                    if (curpos > 0 && newpos < 0) x++;
+                }
+                if (newpos == 0) x++;
+                Console.WriteLine($"Going from {curpos} to {newpos} : passing {x} times by 0");
+                total2 += x;
+                curpos = newpos % 100;
+            }
 
             //Print out total result
             Console.WriteLine(
