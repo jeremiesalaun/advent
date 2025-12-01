cd6c7f910ab2521a5798259eb4466f427567d217
SALAUN Jeremie <jeremie.salaun@soprasteria.com>
2025-12-01 11:11:09 +0100

    fix and clean day 1

--- AdventOfCode2025/Day1.cs
+++ AdventOfCode2025/Day1.cs
@@ -37,10 +37,10 @@ namespace AdventOfCode2025
             long total2 = 0;
             //Read file
             var path = $@"{(TEST ? "Samples" : "Inputs")}\day1.txt";
-            var instructions = File.ReadAllLines(path).Select(s => new turn() { dir = (s[0] == 'L' ? -1 : 1), distance=(int.Parse(s.Substring(1))) }).ToList();
+            var instructions = File.ReadAllLines(path).Select(s => new turn() { dir = (s[0] == 'L' ? -1 : 1), distance = (int.Parse(s.Substring(1))) }).ToList();
 
             var curpos = 50;
-            foreach(var i in instructions)
+            foreach (var i in instructions)
             {
                 curpos = i.move(curpos);
                 if (curpos == 0) total1++;
@@ -49,22 +49,27 @@ namespace AdventOfCode2025
             curpos = 50;
             foreach (var i in instructions)
             {
+                //Since we took care to recalculate curpos to the [0-99] interval, we know that curpos/100=0 which simplifies some formulas.
                 var newpos = i.move2(curpos);
-                int x;
-                if (newpos > curpos)
+                int z = 0;
+                if (i.dir > 0)
                 {
-                    x  = (newpos / 100) - (curpos/100);
-                    if (newpos > 0 && curpos < 0) x++;
+                    z = (newpos / 100);
+                    curpos = newpos - z * 100;
+                }
+                else if (newpos <= 0)
+                {
+                    z = (curpos == 0 ? 0 : 1) - (newpos / 100);
+                    //The trick is to recalculate correctly the new position in negatives : 0 -> 99 -> 98, so a simple a modulo will not do since (-1%100=-1 and not 99)
+                    //curpos = (newpos % 100) + (newpos % 100 < 0 ? 100 : 0);
+                    curpos = (newpos + (z+1) * 100)%100;
                 }
                 else
                 {
-                    x = (curpos / 100) - (newpos / 100);
-                    if (curpos > 0 && newpos < 0) x++;
+                    curpos = newpos;
                 }
-                if (newpos == 0) x++;
-                Console.WriteLine($"Going from {curpos} to {newpos} : passing {x} times by 0");
-                total2 += x;
-                curpos = newpos % 100;
+                //Console.WriteLine($"{curpos} + {i.dir * i.distance} -> {newpos}({p}) : {z}");
+                total2 += z;
             }
 
             //Print out total result
